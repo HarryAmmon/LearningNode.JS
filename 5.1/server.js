cf2ca8f325b7dc2bc4ab61442574d7fcdda1f822
Harry Ammon <harry_a99@hotmail.co.uk>
2018-07-31 18:03:48 +0100

    Halfway through chapter 5

--- 5.1/server.js
+++ 5.1/server.js
@@ -2,6 +2,23 @@ var express = require("express");
 var app = express();
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: "Harry", message: "Message 1"},
+    {name: "Harry", message: "Message 2"},
+    {name: "Harry", message: "Message 3"},
+]
+
+app.get("/messages", (req,res) => {
+    res.send(messages);
+});
+
+app.post("/messages", (req,res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+})
+
+
 var server = app.listen(3000, () => {
     console.log("server is listening on port", server.address().port);
 });
