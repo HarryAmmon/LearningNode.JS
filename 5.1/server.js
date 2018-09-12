95319113618645a6e527f7e417397744e989cbad
Ammonation <harry_a99@hotmail.co.uk>
2018-09-12 12:16:02 +0100

    Don't know what I'm doing now

--- 5.1/server.js
+++ 5.1/server.js
@@ -1,25 +1,36 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
+var messages = [];/*[
     {name: "Harry", message: "Message 1"},
     {name: "Harry", message: "Message 2"},
     {name: "Harry", message: "Message 3"},
-]
+]*/
 
 app.get("/messages", (req,res) => {
     res.send(messages);
 });
 
 app.post("/messages", (req,res) => {
-    console.log(req.body);
+    //console.log(req.body);
+    messages.push(req.body);
+    io.emit('message',req.body);
     res.sendStatus(200);
 })
 
+io.on("connection", (socket) => {
+    console.log("User Connnected");
+})
 
-var server = app.listen(3000, () => {
+
+var server = http.listen(3000, () => {
     console.log("server is listening on port", server.address().port);
 });
 
